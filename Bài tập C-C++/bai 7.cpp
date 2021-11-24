e73c857f05c117bea7e89a00d35dab512ab0828e
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2021-11-25 03:18:38 +0700

    first commit

---
+++ Bài tập C-C++/bai 7.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	int n;
+	float sum = 0;
+	do {
+		printf ("nhap gia tri n = ");
+		scanf ("%d", &n);
+		if (n < 1) {
+			printf ("\nnhap lai gia tri n = ");
+		}
+	} while (n < 1);
+	
+	for (int i=1; i<=n; i++) {
+		sum += (float)i / (i + 1);
+	}
+	
+	printf ("\n");
+	printf ("\nTong = %f", sum);
+	
+	getch();
+	return 0;
+}
