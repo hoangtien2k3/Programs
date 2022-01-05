d88dd68c8c301b14230be7f9ddb138272bdbb979
Hoàng Tiến <91842746+hoangtien2k3@users.noreply.github.com>
2022-01-05 10:26:41 +0700

    Update sap xep tang dan cua mang_void.cpp

--- sap xep tang dan cua mang_void.cpp
+++ sap xep tang dan cua mang_void.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void sortArray(int arr[100], int n) {
-	for (int i=0; i<n; i++) {
+	for (int i=0; i<n-1; i++) {
 		for (int j=i+1; j<n; j++) {
 			if (arr[i] > arr[j]) {
 				int temp = arr[i];
