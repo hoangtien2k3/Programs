e83eda4c812fc98ec5be069adea932e4e84790a7
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-17 09:50:31 +0700

    Upper and Lower

--- String/string_C/strlwr.c
+++ String/string_C/strlwr.c
@@ -6,16 +6,14 @@
 
     Uppercase(chữ Hoa) -> Lowercase(chữ thường)
 */
+//  Cách 1: dùng hàm "strlwr()" (Lowercase)
 #include<stdio.h>
 #include<string.h>
-
-//  Cách 1: dùng hàm "strlwr()" (Lowercase)
 void Strlwr (char s1[]) {
     printf ("Chuoi ban s1 ban dau: %s", s1);
     strlwr (s1);
     printf ("\nChuoi chu Hoa: %s", strlwr(s1));
 }
-
 int main() {
     char s1[20];
     printf ("Nhap vao chuoi s1: ");
@@ -23,4 +21,27 @@ int main() {
     Strlwr(s1);
     return 0;
 }
+
+
+
+// Cách 2: không dùng hàm (vì ASCII (a = A + 32))
+#include<stdio.h>
+#include<string.h> 
+char *solution (char s[]) {
+    int length = strlen(s);
+    for (int i=0; i < strlen(s); i++) {
+        if ('A' <= s[i] && s[i] <= 'Z'){
+            s[i] += 32;
+        }
+    }
+    return s;
+}
+int main() {
+    char s[20];
+    gets(s);
+    solution(s);
+    puts(s);
+
+}
+
 
