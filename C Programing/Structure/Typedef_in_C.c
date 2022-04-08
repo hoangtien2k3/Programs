232530ed974720e2c3202e2a16b766242a4cd8f5
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-04-08 09:39:58 +0700

    Struct 08/04/2022

--- C Programing/Structure/Typedef_in_C.c
+++ C Programing/Structure/Typedef_in_C.c
@@ -21,9 +21,95 @@ int main() {
 
 
 
-////////////////////
+//! ////////////////////////////////////////////////
+//* Cách 1: 
+#include <stdio.h>
+  
+// add is the short name for address
+void compare(int a, int b, int* add_great, int* add_small)
+{
+    if (a > b) {
+  
+        // a is stored in the address pointed
+        // by the pointer variable *add_great
+        *add_great = a;
+        *add_small = b;
+    }
+    else {
+        *add_great = b;
+        *add_small = a;
+    }
+}
+  
+// Driver code
+int main()
+{
+    int great, small, x, y;
+  
+    printf("Enter two numbers: \n");
+    scanf("%d%d", &x, &y);
+  
+    // The last two arguments are passed
+    // by giving addresses of memory locations
+    compare(x, y, &great, &small);
+    printf("\nThe greater number is %d and the"
+           "smaller number is %d",
+           great, small);
+  
+    return 0;
+}
 
 
+//! ///////////////////////////////////////////////////
+//* Cách 2:
+
+//! /////////////////////////////////////////
+// Modified program using structures
+#include <stdio.h>
+struct greaterSmaller {
+    int greater, smaller;
+};
+  
+typedef struct greaterSmaller Struct;
+  
+Struct findGreaterSmaller(int a, int b)
+{
+    Struct s;
+    if (a > b) {
+        s.greater = a;
+        s.smaller = b;
+    }
+    else {
+        s.greater = b;
+        s.smaller = a;
+    }
+  
+    return s;
+}
+  
+// Driver code
+int main()
+{
+    int x, y;
+    Struct result;
+  
+    printf("Enter two numbers: \n");
+    scanf("%d%d", &x, &y);
+  
+    // The last two arguments are passed
+    // by giving addresses of memory locations
+    result = findGreaterSmaller(x, y);
+    printf("\nThe greater number is %d and the"
+           "smaller number is %d",
+           result.greater, result.smaller);
+  
+    return 0;
+}
+
+
+
+
+//! ///////////////////////////////////////
 #include<stdio.h>
 #include<string.h>
 
@@ -59,3 +145,5 @@ int main() {
     
     return 0;
 }
+
+
